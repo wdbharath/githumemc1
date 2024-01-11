a56a8a4116cdfe76124531beb6af139dfae770ab
wdbharath <144111898+wdbharath@users.noreply.github.com>
2024-01-11 13:35:32 +0530

    Update bg.py

--- bg.py
+++ bg.py
@@ -1,1 +1,26 @@
 
+import streamlit as st
+from rembg import remove
+from PIL import Image
+
+
+def removebg(img):
+    input=Image.open(img)
+    return remove(input)
+    
+    
+    
+    
+def main():
+    st.title("Background Remover App")
+    upload=st.file_uploader("Choose an Image...",type=["jpeg","jpg","png"])
+    
+    if upload is not None:
+        st.image(upload,caption="Uploaded Image")
+        st.write("Processing......")
+        result=removebg(upload)
+        st.image(result,caption="Result")
+    
+if __name__=='__main__':
+    main()
+    
